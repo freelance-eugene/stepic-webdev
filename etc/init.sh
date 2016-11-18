600599db23ef018099e8da4c07dddca5ce48e30a
iq1 <seriousjonnik@gmail.com>
2016-11-18 15:01:18 +0200

    Fixed paths

--- etc/init.sh
+++ etc/init.sh
@@ -1,9 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
+sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s ~/web/etc/hello.py /etc/gunicorn.d/hello.py
 gunicorn -c /etc/gunicorn.d/hello.py hello:application &
-
-cd ~/web/ask
-gunicorn wsgi &
+gunicorn ~/web/ask/wsgi &
