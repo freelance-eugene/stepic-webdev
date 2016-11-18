955848cd7b4a44c5616d2ddcb2b38cdf0caf3fc4
iq1 <seriousjonnik@gmail.com>
2016-11-18 19:04:30 +0200

    Config changes

--- etc/init.sh
+++ etc/init.sh
@@ -3,6 +3,7 @@ sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s ~/web/etc/hello.py /etc/gunicorn.d/hello.py
+cd ~/web
 gunicorn -c /etc/gunicorn.d/hello.py hello:application &
 cd ~/web/ask
-gunicorn wsgi &
+gunicorn --bind 0.0.0.0:8000 wsgi &
