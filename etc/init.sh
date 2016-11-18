4e0d61e423fd15987baf4a9f7f59e8f0b1172946
iq1 <seriousjonnik@gmail.com>
2016-11-18 14:09:52 +0200

    Added django project. Changed init and nginx config

--- etc/init.sh
+++ etc/init.sh
@@ -1,2 +1,4 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/lesson.conf
 sudo /etc/init.d/nginx restart
+gunicorn -c /home/box/web/ask/ask/wsgi.py &
