53307451991b8f0de58f4ec3244cfa24d04053ac
iq1 <seriousjonnik@gmail.com>
2016-11-21 16:36:04 +0200

    Fixed typo

--- etc/init.sh
+++ etc/init.sh
@@ -8,7 +8,7 @@ gunicorn -c /etc/gunicorn.d/hello.py hello:application &
 cd ~/web/ask/ask
 gunicorn --bind 0.0.0.0:8000 ask.wsgi:application &
 
-sudo /et/init.d/mysql start
+sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ask;"
 mysql -uroot -e "CREATE USER 'eugene'@'localhost' IDENTIFIED BY 'mypass';"
 mysql -uroot -e "GRANT ALL ON ask.* TO 'eugene'@'localhost'";
