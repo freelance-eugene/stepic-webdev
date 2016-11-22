2c76bb6050fe74a832d70e7dfa3cc73aeeab02b4
iq1 <seriousjonnik@gmail.com>
2016-11-22 12:55:33 +0200

    Changed status field

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateField(auto_now_add=True)
-	rating = models.IntegerField(blank=True)
+	rating = models.IntegerField(blank=True, null=True)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User, related_name='question_author')
 
